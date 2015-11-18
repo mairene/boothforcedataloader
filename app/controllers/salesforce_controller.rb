c5fa9b2cffbb8d41404813fa7540084dd0390e03
Randy Grewal <rgrewalucsb@gmail.com>
2015-11-18 15:54:38 -0800

    changed views 2

--- app/controllers/salesforce_controller.rb
+++ app/controllers/salesforce_controller.rb
@@ -4,19 +4,19 @@ class SalesforceController < ApplicationController
     @areas = Area.all()
   end
 
-  # def area
-  #   if params[:id] =~ /^\d+$/
-  #       @area = Area.find(params[:id])
-  #   else
-  #       @account = Area.find_by_sfid(params[:id])
-  #   end
+  def area
+    if params[:id] =~ /^\d+$/
+        @area = Area.find(params[:id])
+    else
+        @area = Area.find_by_sfid(params[:id])
+    end
 
-  #   begin
-  #     @booths = @area.booths
-  #   rescue
-  #     @booths = []
-  #   end
-  # end
+    begin
+      @booths = @area.booths
+    rescue
+      @booths = []
+    end
+  end
 
   # def booths
   #   @page = (params[:page] || 1).to_i
