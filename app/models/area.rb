90ba3c8b8e31007870b32ed6a60eb38c4d2e738b
Randy Grewal <rgrewalucsb@gmail.com>
2015-11-18 15:59:15 -0800

    changed views 3

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,3 +1,6 @@
 class Area < SalesforceModel
   self.table_name =  'salesforce.area_zone__c'
+
+  has_many :booths, :primary_key => "sfid", :foreign_key => "area_zone__c"
+  has_many :shifts, :primary_key => "sfid", :foreign_key => "area_zone__c"
 end
